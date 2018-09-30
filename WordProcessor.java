f004c47c93e7ed51afca8ecd694c35b6dffbb27b
hpleesz <37176624+hpleesz@users.noreply.github.com>
2018-09-30 15:32:02 +0200

    Update and rename Preprocessor.java to WordProcessor.java

--- WordProcessor.java
+++ WordProcessor.java
@@ -3,20 +3,23 @@ package neural;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Preprocessor {
+public class WordProcessor {
 	private ArrayList<String> words;
 	private ArrayList<String> text;
-	private ArrayList<WordVector> vectors;
+	private ArrayList<double[]> vectors;
 	
-	public Preprocessor(ArrayList<String> input) {
+	public WordProcessor(ArrayList<String> input) {
 		words = new ArrayList<String>(Arrays.asList("de","hogy","azaz", "ugyanis", "eszerint"));
-		//for(int i=0; i<words.size(); i++)
-		//	System.out.print(words.get(i));
 		text = new ArrayList<>();
-		//System.arraycopy(input, 0, text, 0, input.size());
 		for(int i=0; i<input.size(); i++) {
 			text.add(input.get(i));
 		}
+	}
+	
+	public ArrayList<double[]> getWordVectors() {
+		removeWords();
+		makeWordVectors();
+		return vectors;
 	}
 	
 	public void removeWords() {
@@ -31,24 +34,14 @@ public class Preprocessor {
 	public void makeWordVectors() {
 		vectors = new ArrayList<>();
 		for(int i=0; i<text.size(); i++) {
-			//newWordVector(i);
-			//System.out.print(text.size());
 			double j = i+1;
 			double pos = j/text.size();
-			//System.out.print(pos);
 			WordVector wordvec = new WordVector(text.get(i));
 			wordvec.makeVector(pos);
-			vectors.add(wordvec);
+			vectors.add(wordvec.getVector());
 		}
 	}
-	/*
-	public void newWordVector(int i) {
-		double pos = (i+1)/text.size();
-		WordVector wordvec = new WordVector(text.get(i));
-		wordvec.makeVector(pos);
-		vectors.add(wordvec);
-	}
-	*/
+	
 	public ArrayList<String> getWords(){
 		return words;
 	}
@@ -56,14 +49,5 @@ public class Preprocessor {
 	public ArrayList<String> getText(){
 		return text;
 	}
-	
-	public ArrayList<WordVector> getVectors(){
-		return vectors;
-	}
-	
-	public void writeAll() {
-		for(int i=0; i<vectors.size(); i++)
-			vectors.get(i).writeSth();
-			
-	}
+
 }
