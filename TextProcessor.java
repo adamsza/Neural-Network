804c3e17e6d1565580c4ac7dfd7b08217802432f
adamsza <37204132+adamsza@users.noreply.github.com>
2018-10-01 19:10:06 +0200

    Add files via upload

--- TextProcessor.java
+++ TextProcessor.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class TextProcessor {
 	
 	
-	public ArrayList<String> separate(String filename) throws IOException
+	public ArrayList<String> separatewords(String filename) throws IOException
 	{
 		
 		FileReader fr = new FileReader(filename);
@@ -31,5 +31,26 @@ public class TextProcessor {
 		}
 		br.close();
 		return wordlist;
+	}
+	
+	public ArrayList<Double> separatetargets(String filename) throws IOException
+	{
+		FileReader fr = new FileReader(filename);
+		BufferedReader br = new BufferedReader(fr);
+		boolean q = true;
+		String line = br.readLine();
+		ArrayList<Double> targetlist = new ArrayList<Double>();
+		while(q == true)
+		{
+			String[] sp = line.split(" ");
+			for(int i = 0; i < sp.length; i++)
+			{
+				targetlist.add(Double.parseDouble(sp[i]));
+			}
+			line = br.readLine();
+			if(line == null) q = false;
+		}
+		br.close();
+		return targetlist;
 	}
 }
