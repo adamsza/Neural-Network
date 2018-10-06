26647a12ceda55dfb634012e4418edb8072a02a1
hpleesz <37176624+hpleesz@users.noreply.github.com>
2018-10-06 23:34:31 +0200

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -2,6 +2,22 @@ package neural;
 
 public interface Constants {
 
+	//hány szó megy be egyszerre a networkbe
 	public static final int WORDS_WINDOW = 3;
-	public static final int WORD_VECTOR = 5;
+	
+	//milyen hosszú egy szó vektora
+	public static final int WORD_VECTOR = 6;
+	
+	//kimenet opciók száma
+	public static final int NUM_OPTIONS = 7;
+	
+	//neurális magic
+	public static final double ETA = 0.6;
+	
+	//hosszú input vektor
+	public static final int INPUT_VECTOR = WORDS_WINDOW*WORD_VECTOR;
+	
+	//két belső szint
+	public static final int LAYER_1 = 6;
+	public static final int LAYER_2 = 6;
 }
