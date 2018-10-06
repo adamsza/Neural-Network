93c1ca89fc40e28cef9f9626d61279ab2c081c78
hpleesz <37176624+hpleesz@users.noreply.github.com>
2018-10-06 23:38:02 +0200

    Create TargetProcessor.java

---
+++ TargetProcessor.java
@@ -0,0 +1,32 @@
+package neural;
+
+import java.util.ArrayList;
+
+public class TargetProcessor implements Constants{
+
+	public TargetProcessor() {
+		
+	}
+	
+	//integer listából válasz vektor dolgokat csinál
+	//0: semmi, 1: sz. fajta, 2: név, 3: személyi szám, 4: adószám, 5: cím, 6: dátum
+	//0 -> 1000000, 1 -> 0100000, stb
+	public ArrayList<double[]> makeTargets(ArrayList<Integer> targets){
+		
+		ArrayList<double[]> newtargets = new ArrayList<double[]>();
+		for(int i = 0; i<targets.size(); i++) {
+			int digit = targets.get(i);
+			double[] vector = new double[NUM_OPTIONS];
+			for(int j = 0; j<digit; j++) {
+				vector[j] = 0;
+			}
+			vector[digit] = 1;
+			for(int k = digit+1; k<NUM_OPTIONS; k++) {
+				vector[k] = 0;
+			}
+			newtargets.add(vector);
+		}
+		return newtargets;
+	}
+			
+}
