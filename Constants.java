cbf59f36b8befcbf70fbf23b1af9a3a632ada05c
hpleesz <37176624+hpleesz@users.noreply.github.com>
2018-11-03 19:07:46 +0100

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -9,20 +9,24 @@ public interface Constants {
 	public static final int WORD_VECTOR = 5;
 	
 	//kimenet opciók száma
-	public static final int NUM_OPTIONS = 2;
+	public static final int NUM_OPTIONS = 7;
+	public static final int NUM_NETWORK_OPTIONS = 2;
 	
-	public static final int NUM_NETWORK = 2;
-		
+	public static final int NUM_NETWORK = NUM_OPTIONS-1;
+	
 	//neurális magic
-	public static final double ETA = 0.6;
+	public static final double ETA = 0.5;
 	
 	//hosszú input vektor
 	public static final int INPUT_VECTOR = WORDS_WINDOW*WORD_VECTOR;
 	
 	//két belső szint
-	public static final int LAYER_1 = 10;
-	public static final int LAYER_2 = 5;
-	
+	public static final int LAYER_1 = 7;
+	public static final int LAYER_2 = 7;
+
+
+			
 	//ennel jobb kell hogy legyen a tipp
-	public static final double BOUNDARY = 0.3;
+	public static final double BOUNDARY = 0.6;
+	public static final double YES_NO_BOUNDARY = 0.5;
 }
